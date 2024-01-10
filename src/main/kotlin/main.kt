11d6d18f5ab141c52ce956b03ae967de4859036f
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-01-10 18:02:33 +0100

    Measure only MEDIUM, MEDIUM_TO_HIGH and HIGH compression levels

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,7 @@ package ddol.compression.tests
 
 import ddol.compression.tests.archivers.Archiver
 import java.io.File
+import kotlin.enums.enumEntries
 import kotlin.time.measureTimedValue
 
 fun main() {
@@ -14,45 +15,48 @@ fun main() {
         Archiver.all.forEach archiver@{ archiver ->
             if (archiver.state is Archiver.State.Unavailable) return@archiver
 
-            GenericCompressionLevel.entries.forEach { genericCompressionLevel ->
-                for (round in 1..Environment.rounds) {
-                    println()
-                    println("=== ${archiver.name}, compression $genericCompressionLevel, payload '${originalPayload.path.name}', round $round of ${Environment.rounds} ===")
+            @OptIn(ExperimentalStdlibApi::class)
+            enumEntries<GenericCompressionLevel>()
+                .dropWhile { it < GenericCompressionLevel.MEDIUM }
+                .forEach { genericCompressionLevel ->
+                    for (round in 1..Environment.rounds) {
+                        println()
+                        println("=== ${archiver.name}, compression $genericCompressionLevel, payload '${originalPayload.path.name}', round $round of ${Environment.rounds} ===")
 
-                    val specificCompressionLevel = archiver.computeSpecificCompressionLevel(genericCompressionLevel)
-                    val compressedPayloadDir = prepareCompressedPayloadDir()
-                    val (compressedPayload, compressionDuration) = measureTimedValue {
-                        archiver.compress(specificCompressionLevel, originalPayload, compressedPayloadDir)
+                        val specificCompressionLevel = archiver.computeSpecificCompressionLevel(genericCompressionLevel)
+                        val compressedPayloadDir = prepareCompressedPayloadDir()
+                        val (compressedPayload, compressionDuration) = measureTimedValue {
+                            archiver.compress(specificCompressionLevel, originalPayload, compressedPayloadDir)
+                        }
+                        val compressionRatio = CompressionRatio(originalPayload.size, compressedPayload.size)
+
+                        println("    Compression:")
+                        println("        Ratio:        ${compressionRatio.ratioAsText}")
+                        println("        Space saving: ${compressionRatio.spaceSavingAsText}")
+                        println("        Duration:     $compressionDuration")
+
+                        val uncompressedPayloadDir = prepareUncompressedPayloadDir()
+                        val compressedPayloadInUncompressedPayloadDir = compressedPayload.copyTo(uncompressedPayloadDir)
+                        val (uncompressedPayload, uncompressionDuration) = measureTimedValue {
+                            archiver.uncompress(compressedPayloadInUncompressedPayloadDir, uncompressedPayloadDir)
+                        }
+
+                        check(uncompressedPayload.name == originalPayload.name)
+                        check(uncompressedPayload.size == originalPayload.size)
+
+                        println("    Uncompression:")
+                        println("        Duration:     $uncompressionDuration")
+
+                        report.report(
+                            archiver = archiver,
+                            originalPayload = originalPayload,
+                            specificCompressionLevel = specificCompressionLevel,
+                            compressionRatio = compressionRatio,
+                            compressionDuration = compressionDuration,
+                            uncompressionDuration = uncompressionDuration
+                        )
                     }
-                    val compressionRatio = CompressionRatio(originalPayload.size, compressedPayload.size)
-
-                    println("    Compression:")
-                    println("        Ratio:        ${compressionRatio.ratioAsText}")
-                    println("        Space saving: ${compressionRatio.spaceSavingAsText}")
-                    println("        Duration:     $compressionDuration")
-
-                    val uncompressedPayloadDir = prepareUncompressedPayloadDir()
-                    val compressedPayloadInUncompressedPayloadDir = compressedPayload.copyTo(uncompressedPayloadDir)
-                    val (uncompressedPayload, uncompressionDuration) = measureTimedValue {
-                        archiver.uncompress(compressedPayloadInUncompressedPayloadDir, uncompressedPayloadDir)
-                    }
-
-                    check(uncompressedPayload.name == originalPayload.name)
-                    check(uncompressedPayload.size == originalPayload.size)
-
-                    println("    Uncompression:")
-                    println("        Duration:     $uncompressionDuration")
-
-                    report.report(
-                        archiver = archiver,
-                        originalPayload = originalPayload,
-                        specificCompressionLevel = specificCompressionLevel,
-                        compressionRatio = compressionRatio,
-                        compressionDuration = compressionDuration,
-                        uncompressionDuration = uncompressionDuration
-                    )
                 }
-            }
         }
     }
 
